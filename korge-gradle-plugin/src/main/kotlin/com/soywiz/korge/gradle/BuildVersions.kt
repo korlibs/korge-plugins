4a000324e1069bc9ea9d9c6f51b9fe98119e5cd8
soywiz <soywiz@gmail.com>
2019-12-01 05:27:35 +0100

    1.4.1

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -6,10 +6,10 @@ object BuildVersions {
 	const val KDS = "1.9.0"
 	const val KMEM = "1.9.0"
 	const val KORMA = "1.9.0"
-	const val KORIO = "1.9.0"
+	const val KORIO = "1.9.1"
 	const val KORIM = "1.9.0"
 	const val KORAU = "1.9.0"
-	const val KORGW = "1.9.1"
+	const val KORGW = "1.9.2"
 	const val KORGE = "1.4.0"
 	const val KOTLIN = "1.3.60"
 }
