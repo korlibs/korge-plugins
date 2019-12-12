faab907365fb4704236f5d41d911620196bb4880
soywiz <soywiz@gmail.com>
2019-12-12 20:12:14 +0100

    Publish https://bintray.com/korlibs/korlibs/korge-plugins

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -10,7 +10,7 @@ object BuildVersions {
 	const val KORIM = "1.9.2"
 	const val KORAU = "1.9.6"
 	const val KORGW = "1.9.9"
-	const val KORGE = "1.5.0"
+	const val KORGE = "1.5.1"
 	const val KOTLIN = "1.3.61"
 	const val COROUTINES = "1.3.2-1.3.60"
 }
