6c4311d5a34c09c9df8016215ed225fb55f95562
soywiz <soywiz@gmail.com>
2020-01-12 19:40:36 +0100

    Bump Versions - SNAPSHOT

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -2,15 +2,15 @@
 package com.soywiz.korge.gradle
 
 object BuildVersions {
-	const val KLOCK = "1.8.2"
+	const val KLOCK = "1.8.6"
 	const val KDS = "1.9.2"
 	const val KMEM = "1.9.1"
 	const val KORMA = "1.9.1"
-	const val KORIO = "1.9.6"
-	const val KORIM = "1.9.2"
-	const val KORAU = "1.9.6"
-	const val KORGW = "1.9.9"
-	const val KORGE = "1.5.1"
+	const val KORIO = "1.9.8"
+	const val KORIM = "1.9.6"
+	const val KORAU = "1.9.8"
+	const val KORGW = "1.9.11"
+	const val KORGE = "1.5.4-SNAPSHOT"
 	const val KOTLIN = "1.3.61"
-	const val COROUTINES = "1.3.2-1.3.60"
+	const val COROUTINES = "1.3.3"
 }
