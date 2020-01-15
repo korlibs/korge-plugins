0eefc28efa33e9f7bb1a9cba11f1593ff2c165f0
soywiz <soywiz@gmail.com>
2020-01-15 21:24:28 +0100

    Fixes https://github.com/korlibs/korge/issues/68

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -2,7 +2,7 @@
 package com.soywiz.korge.gradle
 
 object BuildVersions {
-	const val KLOCK = "1.8.6"
+	const val KLOCK = "1.8.5"
 	const val KDS = "1.9.2"
 	const val KMEM = "1.9.1"
 	const val KORMA = "1.9.1"
@@ -10,7 +10,7 @@ object BuildVersions {
 	const val KORIM = "1.9.6"
 	const val KORAU = "1.9.8"
 	const val KORGW = "1.9.11"
-	const val KORGE = "1.5.4"
+	const val KORGE = "1.5.5"
 	const val KOTLIN = "1.3.61"
 	const val COROUTINES = "1.3.3"
 }
