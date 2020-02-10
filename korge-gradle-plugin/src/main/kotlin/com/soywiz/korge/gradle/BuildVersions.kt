0ce3d56ca80ad20cf385028968bb26dd978f6de1
soywiz <soywiz@gmail.com>
2020-02-10 22:00:45 +0100

    1.5.6.3: Bump Korau version

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -8,7 +8,7 @@ object BuildVersions {
 	const val KORMA = "1.9.1"
 	const val KORIO = "1.9.8"
 	const val KORIM = "1.9.7"
-	const val KORAU = "1.9.9"
+	const val KORAU = "1.9.10"
 	const val KORGW = "1.9.11"
 	const val KORGE = "1.5.6"
 	const val KOTLIN = "1.3.61"
