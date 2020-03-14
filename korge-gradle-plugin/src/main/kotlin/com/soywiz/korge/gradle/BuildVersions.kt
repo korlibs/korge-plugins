970d7d1e2b36c13c238bfa07195c3552ab19a470
soywiz <soywiz@gmail.com>
2020-03-14 21:32:37 +0100

    1.11.1.0

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -10,7 +10,7 @@ object BuildVersions {
 	const val KORIM = "1.10.0"
 	const val KORAU = "1.11.0"
 	const val KORGW = "1.10.0"
-	const val KORGE = "1.11.0"
+	const val KORGE = "1.11.1"
 	const val KOTLIN = "1.3.70"
 	const val COROUTINES = "1.3.4"
 	const val ANDROID_BUILD = "3.6.0"
