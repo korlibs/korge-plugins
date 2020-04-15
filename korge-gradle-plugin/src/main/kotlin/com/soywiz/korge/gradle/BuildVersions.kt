d9deacadeb2853c693f811e11af670d2ff20186e
soywiz <soywiz@gmail.com>
2020-04-15 05:00:11 +0200

    Bump kotlin version

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -7,11 +7,11 @@ object BuildVersions {
 	const val KMEM = "1.10.2"
 	const val KORMA = "1.11.4"
 	const val KORIO = "1.10.3"
-	const val KORIM = "1.12.2"
+	const val KORIM = "1.12.3"
 	const val KORAU = "1.11.1"
 	const val KORGW = "1.12.0"
 	const val KORGE = "1.12.0"
-	const val KOTLIN = "1.3.71"
+	const val KOTLIN = "1.3.72"
 	const val COROUTINES = "1.3.5"
 	const val ANDROID_BUILD = "3.6.0"
 }
