967c8e9ea181156ae3f2a9fad8d2572764c81d2f
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-05-03 17:49:20 +0200

    Missing versions

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -2,15 +2,15 @@
 package com.soywiz.korge.gradle
 
 object BuildVersions {
-	const val KLOCK = "1.10.6"
+	const val KLOCK = "1.11.1"
 	const val KDS = "1.10.9"
-	const val KMEM = "1.10.3"
-	const val KORMA = "1.11.7"
-	const val KORIO = "1.10.6"
-	const val KORIM = "1.12.9"
-	const val KORAU = "1.11.6"
-	const val KORGW = "1.12.5"
-	const val KORGE = "1.12.2"
+	const val KMEM = "1.10.4"
+	const val KORMA = "1.11.9"
+	const val KORIO = "1.11.0"
+	const val KORIM = "1.12.10"
+	const val KORAU = "1.11.7"
+	const val KORGW = "1.12.6"
+	const val KORGE = "1.12.3"
 	const val KOTLIN = "1.3.72"
 	const val COROUTINES = "1.3.5"
 	const val ANDROID_BUILD = "3.6.0"
