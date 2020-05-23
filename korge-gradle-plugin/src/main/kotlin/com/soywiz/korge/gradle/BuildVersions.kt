870cbade9c886c77e2079b8cde82c3a33bb86fac
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-05-23 14:13:55 +0200

    1.12.7.0

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -2,16 +2,16 @@
 package com.soywiz.korge.gradle
 
 object BuildVersions {
-	const val KLOCK = "1.11.1"
+	const val KLOCK = "1.11.3"
 	const val KDS = "1.10.12"
-	const val KMEM = "1.10.4"
-	const val KORMA = "1.11.14"
-	const val KORIO = "1.11.2"
-	const val KORIM = "1.12.14"
-	const val KORAU = "1.11.7"
-	const val KORGW = "1.12.8"
-	const val KORGE = "1.12.6"
+	const val KMEM = "1.10.5"
+	const val KORMA = "1.11.15"
+	const val KORIO = "1.11.3"
+	const val KORIM = "1.12.19"
+	const val KORAU = "1.11.8"
+	const val KORGW = "1.12.9"
+	const val KORGE = "1.12.7"
 	const val KOTLIN = "1.3.72"
-	const val COROUTINES = "1.3.5"
+	const val COROUTINES = "1.3.7"
 	const val ANDROID_BUILD = "3.6.0"
 }
