75dfeab569d159cd82a03d8b7f78c8e1b14091d5
soywiz <soywiz@gmail.com>
2020-11-23 13:55:42 +0100

    Bump korge version

--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/BuildVersions.kt
@@ -10,7 +10,7 @@ object BuildVersions {
 	const val KORIM = "2.0.0"
 	const val KORAU = "2.0.0"
 	const val KORGW = "2.0.0"
-	const val KORGE = "2.0.0"
+	const val KORGE = "2.0.1"
 	const val KOTLIN = "1.4.20"
     const val JNA = "5.6.0"
 	const val COROUTINES = "1.4.1"
